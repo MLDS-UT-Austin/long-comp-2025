b07f4b2cff821d4de31260b89751a83ca6f85bbd
justinsasek <justinsasek@gmail.com>
2024-09-21 12:09:50 -0500

    cleaned up testing simulation

--- simulation.py
+++ simulation.py
@@ -22,7 +22,6 @@ class Simulation:
     nlp: NLP
     submission_paths: str = "agents/**/submission.py"
     gave_save_dir: str = "games/simulation0"
-    n_games: int = 100
     team_size: int = 2
     n_rounds: int = 20
 
@@ -38,13 +37,17 @@ class Simulation:
                 print(f"Agent {name} failed validation: {e}")
                 raise e
 
-    async def run(self):
+    async def run(self, n_games: int = 1, agent_names: list[str] | None = None):
+        if agent_names is None:
+            agent_classes = list(self.agent_registry.values())
+        else:
+            agent_classes = [self.agent_registry[agent_name] for agent_name in agent_names]
         player_classes = [
-            random.sample(list(self.agent_registry.values()), self.team_size)
-            for _ in range(self.n_games)
+            random.sample(agent_classes, self.team_size)
+            for _ in range(n_games)
         ]
         tqdm_bar = tqdm(
-            total=self.n_games * self.n_rounds, desc="Rounds", colour="green"
+            total=n_games * self.n_rounds, desc="Rounds", colour="green"
         )
         games = [Game(agents, self.nlp, self.n_rounds) for agents in player_classes]
         for game in games:
@@ -83,7 +86,7 @@ class Simulation:
 
     def get_scores(self) -> pd.DataFrame:
         self.games, "must call run() first"
-        df = pd.DataFrame(index=range(self.n_games), columns=self.agent_registry.keys())
+        df = pd.DataFrame(index=range(len(self.games)), columns=self.agent_registry.keys())
 
         for i, game in enumerate(self.games):
             scores = game.get_scores()
@@ -149,6 +152,7 @@ class Simulation:
 
 
 if __name__ == "__main__":
+    # Select the NLP model to use
     # # Use Together.ai for Llama and BERT
     # nlp = NLP(llm = Llama(), embedding=BERTTogether())
     # # Use Together.ai for Llama but run BERT locally
@@ -156,24 +160,31 @@ if __name__ == "__main__":
     # Output the same string for every question and return a 0 array for every embedding
     nlp = NLP(llm=DummyLLM(), embedding=DummyEmbedding())
 
-    # Demo at Long Comp Intro Day
-    import_agent_from_file("agents/submission.py")
-    game = Game(list(AGENT_REGISTRY.values()), nlp, 20)
-    asyncio.run(game.play())
-    game.get_scores()
-    game.render()
 
-    # Long Comp Day
-    # Show teams over time
+    # Test your agent
     sim = Simulation(nlp)
-    asyncio.run(sim.run())
-    sim.pickle_games()
-    sim.load_games()
-    print(sim.get_scores())
-    sim.visualize_scores()
-    sim.save_visualization("simulation.mp4")
+    import_agent_from_file("agents/submission.py")
+    asyncio.run(sim.run(n_games=10, agent_names=["ExampleAgent", "TeamNameHere"]))
+    print("Average Scores:", sim.get_scores().mean(axis=0))
 
-    # Show final teams
-    team_names = ["ExampleAgent", "TeamNameHere"]
-    player_classes = [AGENT_REGISTRY[name] for name in team_names]
-    game = Game(player_classes, nlp, 20)
+    # # Demo at Long Comp Intro Day
+    # import_agent_from_file("agents/submission.py")
+    # game = Game(list(AGENT_REGISTRY.values()), nlp, 20)
+    # asyncio.run(game.play())
+    # game.get_scores()
+    # game.render()
+
+    # # Long Comp Day
+    # # Show teams over time
+    # sim = Simulation(nlp)
+    # asyncio.run(sim.run())
+    # sim.pickle_games()
+    # sim.load_games()
+    # print("Average Scores:", sim.get_scores().mean(axis=0))
+    # sim.visualize_scores()
+    # sim.save_visualization("simulation.mp4")
+
+    # # Show final teams
+    # team_names = ["ExampleAgent", "TeamNameHere"]
+    # player_classes = [AGENT_REGISTRY[name] for name in team_names]
+    # game = Game(player_classes, nlp, 20)
